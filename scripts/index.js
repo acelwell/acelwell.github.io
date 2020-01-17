21b6d9f3012be0b5956c661ba154428bc995705e
krytality <krytality@hotmail.com>
2020-01-16 20:07:00 -0800

    tyring to add react

---
+++ scripts/index.js
@@ -0,0 +1,39 @@
+'use strict';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+import {App} from 'App.js';
+// import * as serviceWorker from './serviceWorker';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
+
+
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+const domContainer = document.querySelector('#root');
+ReactDOM.render(e(LikeButton), domContainer);
